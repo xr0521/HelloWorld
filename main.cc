4e1302075e03a380090c28bee6d9ebefafccffb6
afei <15063871956@163.com>
2025-11-06 09:14:25 +0800

    初始提交

---
+++ main.cc
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+void sort(int[] arr,int length){
+
+}
+int main(){
+    int arr[]={1,3,5,21,66,0,9};
+    int length=sizeof(arr)/sizeof(arr[0]);
+    sort(arr,length);
+    return 0;
+}
